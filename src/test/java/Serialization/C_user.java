130dd57415f51bf5b52efcf626f86ea9c8f4ff70
Vignesh Vellingiri <vignesh.vellingiri28@gmail.com>
2025-06-01 19:05:50 +0530

    serialization

---
+++ src/test/java/Serialization/C_user.java
@@ -0,0 +1,25 @@
+package Serialization;
+
+public class C_user {
+	
+	private String name;
+	private String job;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/Serialization/Create_user_Using_Serialization.java
@@ -0,0 +1,29 @@
+package Serialization;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class Create_user_Using_Serialization {
+	
+	@Test
+	public void createUser() {
+		
+		C_user user = new C_user();
+		
+		user.setName("Vignesh");
+		user.setJob("SDET");
+		
+		RestAssured.given()
+		              .contentType(ContentType.JSON)
+		              .header("x-api-key","reqres-free-v1")
+		              .body(user)
+		              .when()
+		              .baseUri("https://reqres.in/api/users")
+		              .post()
+		              .then().log().body()
+		              .statusCode(201);
+	}
+
+}
